0f2e143805ed28364c00efe4a84452f61ea904bf
Madhur Shrimal <madhur.shrimal@gmail.com>
2015-11-26 13:09:50 -0800

    added recommender

--- src/main/java/spark_example/matrix_decomposition/MatrixMultiplication.java
+++ src/main/java/spark_example/matrix_decomposition/MatrixMultiplication.java
@@ -1,5 +1,8 @@
 package spark_example.matrix_decomposition;
 
+import java.util.List;
+import java.util.regex.Pattern;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -7,9 +10,11 @@ import org.apache.spark.api.java.function.Function;
 import org.apache.spark.mllib.linalg.DenseMatrix;
 import org.apache.spark.mllib.linalg.Matrix;
 import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.mllib.linalg.distributed.IndexedRow;
+import org.apache.spark.mllib.linalg.distributed.IndexedRowMatrix;
 import org.apache.spark.mllib.linalg.distributed.RowMatrix;
 
-import java.util.regex.Pattern;
+import scala.Function1;
 
 
 public final class MatrixMultiplication {
@@ -26,6 +31,7 @@ public final class MatrixMultiplication {
 
 
     JavaRDD<String> distFile = sc.textFile("res/matrix");
+    
     JavaRDD<org.apache.spark.mllib.linalg.Vector> movRDD = distFile.map(new Function<String, org.apache.spark.mllib.linalg.Vector>() {
       public org.apache.spark.mllib.linalg.Vector call(String s) throws Exception {
         String[] sa = s.split(",");
@@ -36,7 +42,56 @@ public final class MatrixMultiplication {
         return Vectors.dense(da);
       }
     });
+//    int count = 0;
+    JavaRDD<IndexedRow> indexedRDD = distFile.map(new Function<String, IndexedRow>() {
+      public IndexedRow call(String s) throws Exception {
+        String[] sa = s.split(",");
+        double[] da = new double[sa.length];
+        for(int i = 0; i < sa.length; i++) {
+          da[i] = Double.valueOf(sa[i]);
+        }
+        return new IndexedRow((long)da[0], Vectors.dense(da));
+      } 
+    });
+    
+    JavaRDD<String> distFile1 = sc.textFile("res/new");
+    JavaRDD<IndexedRow> newindexedRDD = distFile1.map(new Function<String, IndexedRow>() {
+        public IndexedRow call(String s) throws Exception {
+          String[] sa = s.split(",");
+          double[] da = new double[sa.length];
+          for(int i = 0; i < sa.length; i++) {
+            da[i] = Double.valueOf(sa[i]);
+          }
+          return new IndexedRow((long)da[0], Vectors.dense(da));
+        } 
+      });
+    
+    indexedRDD.union(newindexedRDD);
+    
+    JavaRDD<IndexedRow> i = indexedRDD.filter(new Function<IndexedRow, Boolean>() {
+
+		@Override
+		public Boolean call(IndexedRow v1) throws Exception {
+			// TODO Auto-generated method stub
+			
+			return v1.index() == 2;
+		}
+    	
+    });
+    
+    List<IndexedRow> list = i.collect();
+    org.apache.spark.mllib.linalg.Vector v = list.get(0).vector();
+    double[] vec = v.toArray();
+    System.out.println("Vector");
+    for(int j=0; j < vec.length; j++){
+    	System.out.println(vec[j]);
+    }
+    IndexedRowMatrix indexedRowMatrix = new IndexedRowMatrix(indexedRDD.rdd());
+    
+    
+    
     RowMatrix movieMatrix = new RowMatrix(movRDD.rdd());
+    
     System.out.println("movieMatrix: " + movieMatrix.numRows() + " rows, " + movieMatrix.numCols() + " cols.");
 
     Matrix userMatrix = new DenseMatrix(5, 2, new double[] {1, 1, 1, 1, 1, 2, 2, 2, 2, 2});
---
+++ src/main/java/spark_example/matrix_decomposition/SparkMultiplication.java
@@ -0,0 +1,102 @@
+package spark_example.matrix_decomposition;
+
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.mllib.linalg.DenseVector;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.distributed.IndexedRow;
+
+import scala.Tuple2;
+
+public class SparkMultiplication {
+	public static void main(String[] args) {
+		SparkConf sparkConf = new SparkConf().setAppName("App").setMaster("local[3]");
+	    JavaSparkContext sc = new JavaSparkContext(sparkConf);
+	    
+	    JavaRDD<String> usersFile = sc.textFile("res/users");
+	    
+	    JavaRDD<IndexedRow> usersRDD = usersFile.map(new Function<String, IndexedRow>(){
+
+			@Override
+			public IndexedRow call(String v1) throws Exception {
+				String[] arr = v1.split(",");
+				long key = Long.parseLong(arr[0]);
+				int len = arr.length;
+				double[] vec = new double[len-1];
+				for(int i=0; i < len-1; i++){
+					vec[i] = Double.parseDouble(arr[i+1]);
+				}
+				Vector v = new DenseVector(vec);
+				
+				return new IndexedRow(key, v);
+			}
+	    	
+	    });
+	    
+	    
+	    JavaRDD<String> moviesFile = sc.textFile("res/movies");
+	    
+	    JavaRDD<IndexedRow> moviesRDD = moviesFile.map(new Function<String, IndexedRow>(){
+
+			@Override
+			public IndexedRow call(String v1) throws Exception {
+				String[] arr = v1.split(",");
+				long key = Long.parseLong(arr[0]);
+				int len = arr.length;
+				double[] vec = new double[len-1];
+				for(int i=0; i < len-1; i++){
+					vec[i] = Double.parseDouble(arr[i+1]);
+				}
+				Vector v = new DenseVector(vec);
+				
+				return new IndexedRow(key, v);
+			}
+	    	
+	    });
+	    
+	    final long userId = 2;
+	    JavaRDD<IndexedRow> a = usersRDD.filter(new Function<IndexedRow, Boolean>() {
+
+			@Override
+			public Boolean call(IndexedRow v1) throws Exception {
+				
+				return v1.index() == userId;
+			}
+		});
+	    final double[] user = a.first().vector().toArray();
+	    printDoubleArr("user", user);
+	    
+	    JavaRDD<Tuple2<Long, Double>> result = moviesRDD.map(new Function<IndexedRow, Tuple2<Long, Double>>() {
+
+			@Override
+			public Tuple2<Long, Double> call(IndexedRow v1) throws Exception {
+				double[] movie = v1.vector().toArray();
+				
+				long key = v1.index();
+				printDoubleArr("movie"+key , movie);
+				Double val = VectorMath.dot(user, movie);
+				return new Tuple2<Long, Double>(key, val);
+			}
+		});
+	    
+	    List<Tuple2<Long, Double>> ret = result.collect();
+	    for(Tuple2<Long, Double> curr: ret){
+	    	System.out.println(curr._1 + " " + curr._2);
+	    }
+	    
+	    sc.stop();
+	}
+	
+	public static void printDoubleArr(String name, double[] arr){
+		System.out.println(name);
+	    for(int i=0; i < arr.length; i++){
+	    	System.out.print(arr[i] + " ");
+	    }
+	    System.out.println();
+	}
+	
+}
